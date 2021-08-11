a5e28045c6cbedb731ab2df5972ad7e0d2771a1e
Weijia Sun <49627905+thisisweijia@users.noreply.github.com>
2021-08-11 15:33:29 +0800

    Update

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b695a4f01ab273f5109a.js"
+    "url": "webpack-runtime-bab4c3c707eb4be520ba.js"
   },
   {
     "url": "framework-0c99cb064d432704945a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "905149b1f6fcfd7e129d593b73c6e563"
+    "revision": "af7c3fa792cdeebc679e2b9b256a4a6e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-458ab40572c80fd02a74.js"
